73bf1a217945722992eccdc5514ec470e801cd86
Ivo Alxneit <ivo.alxneit@psi.ch>
2020-12-15 10:25:25 +0100

    Increase version number
    Signed-off-by: Ivo Alxneit <ivo.alxneit@psi.ch>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='JEOL_eds',
       description='Read binary ".pts" files',
-      version='0.4',
+      version='0.5',
       author='Ivo Alxneit',
       author_email='ivo.alxneit@psi.ch',
       packages=['JEOL_eds'],
